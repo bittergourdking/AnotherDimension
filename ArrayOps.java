82e066d0a791837f3c20b9a63a48360c2a7b3a34
bittergourdking <jkang21@stuy.edu>
2020-10-23 12:15:01 -0400

    largest working now!

--- ArrayOps.java
+++ ArrayOps.java
@@ -7,5 +7,15 @@ public class ArrayOps {
     return answer;
   }
 
+  public static int largest(int[] arr) {
+    int answer = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > answer) {
+        answer = arr[i];
+      }
+    }
+    return answer;
+  }
+
 
 }
--- Tester.java
+++ Tester.java
@@ -1,9 +1,15 @@
 public class Tester {
   public static void main(String[] args) {
     //for sum
-    int[]one  =   { 1, 3, 5 };
-    int[]two  =   { 10, 20, 30, 40, 50 };
+    int[]one  = { 1, 3, 5 };
+    int[]two  = { 10, 20, 30, 40, 50 };
+    int[]three = { };
     System.out.println(ArrayOps.sum(one));
     System.out.println(ArrayOps.sum(two));
+    System.out.println(ArrayOps.sum(three));
+    //for largest
+    System.out.println(ArrayOps.largest(one));
+    System.out.println(ArrayOps.largest(two));
+    System.out.println(ArrayOps.largest(three));
   }
 }
