d9bf6dc61048db47448d0b5236f835240a4c3d97
bittergourdking <jkang21@stuy.edu>
2020-10-23 12:31:41 -0400

    sumRows workin!

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,4 +1,15 @@
 public class ArrayOps {
+  public static String toString(int[] arr) {
+    if (arr.length == 0) {
+    return "{}";
+    }
+    String answer = "{";
+    for (int i = 0; i < arr.length - 1; i++) {
+      answer = answer + arr[i] + ", ";
+    }
+    answer = answer + arr[arr.length - 1] + "}";
+    return answer;
+  }
   public static int sum(int[] arr) {
     int answer = 0;
     for (int i = 0; i < arr.length; i++) {
@@ -6,7 +17,6 @@ public class ArrayOps {
     }
     return answer;
   }
-
   public static int largest(int[] arr) {
     int answer = 0;
     for (int i = 0; i < arr.length; i++) {
@@ -16,6 +26,15 @@ public class ArrayOps {
     }
     return answer;
   }
+
+  public static int[] sumRows(int[][] matrix) {
+    int[]answer = new int[matrix.length];
+    for (int i = 0; i < answer.length; i++) {
+      answer[i] = sum(matrix[i]);
+    }
+    return answer;
+  }
+
 
 
 }
--- Tester.java
+++ Tester.java
@@ -4,6 +4,10 @@ public class Tester {
     int[]one  = { 1, 3, 5 };
     int[]two  = { 10, 20, 30, 40, 50 };
     int[]three = { };
+    int[][]four = {  {  1,  0, 12, -1 },
+                  {  7, -2,  2,  1 },
+                  { -5, -2,  2, -9 } };
+    int[][]five = { };
     System.out.println(ArrayOps.sum(one));
     System.out.println(ArrayOps.sum(two));
     System.out.println(ArrayOps.sum(three));
@@ -11,5 +15,8 @@ public class Tester {
     System.out.println(ArrayOps.largest(one));
     System.out.println(ArrayOps.largest(two));
     System.out.println(ArrayOps.largest(three));
+    //for sumRows
+    System.out.println(ArrayOps.toString(ArrayOps.sumRows(four)));
+    System.out.println(ArrayOps.toString(ArrayOps.sumRows(five)));
   }
 }
